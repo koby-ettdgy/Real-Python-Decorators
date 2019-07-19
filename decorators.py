0d48c51c2f8601fc13b98b37cb98ddefe970c508
koby Ettdgy <kobye@bynet.co.il>
2019-07-19 08:13:30 +0300

    debug function decorator

--- decorators.py
+++ decorators.py
@@ -1,4 +1,5 @@
 import functools
+import time
 
 def decorators(func):
 	@functools.wraps(func)
@@ -9,3 +10,31 @@ def decorators(func):
 		return value
 	return wrappers_decorators
 
+def timer(func):
+	""" Print the runtine of the decorated function"""
+	@functools.wraps(func)
+	def wrappers_timer(*args, **kwargs):
+		start_time = time.perf_counter() # 1
+		value = func(*args, **kwargs)
+		end_time = time.perf_counter()
+		run_time = end_time - start_time
+		print (f'Finished {func.__name__!r} in {run_time:.4f} secs')
+		return value
+	return wrappers_timer
+
+
+
+def debug(func):
+	""" Print the function signature and return a value """
+	@functools.wraps(func)
+	def wrappers_debug(*args, **kwargs):
+		# Do something before
+		args_repr = [repr(a) for a in args]
+		kwargs_repr = [f"{k} = {v!r}" for k, v in kwargs.items()]
+		signature = ", ".join(args_repr + kwargs_repr)
+		print (f"Calling {func.__name__}({signature})")
+		value = func(*args, **kwargs)
+		# Do something after
+		print(f"{func.__name__!r} returned {value!r}")
+		return value
+	return wrappers_debug
--- examples.py
+++ examples.py
@@ -5,3 +5,12 @@ def waste_some_time(num_times):
 	for _ in range(num_times):
 		sum([i**2 for i in range(10000)])
 
+
+@debug
+def make_greeting(name, age=None):
+	if age is None:
+		return f"Howdy {name}"
+	else:
+		return f"Whoa {name}! {age} already, you are growing up!"
+
+
